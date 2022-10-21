37902dbee10749a1faebb3f2417335241625bcce
Dustin Kaban <dustin@voodoo.io>
2022-10-21 07:22:40 -0600

    left rotation of a list solution

---
+++ Arrays/LeftRotationResult.cs
@@ -0,0 +1,64 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class LeftRotationResult
+{
+
+    /*
+     * Challenge: Rotate a given list to the left d amount of times.
+     *
+     * Complete the 'rotLeft' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER d
+     */
+
+    public static List<int> rotLeft(List<int> a, int d)
+    {
+        var tempList = a;
+        LinkedList<int> linkedList = new LinkedList<int>(tempList);
+        
+        for(int i=0;i<d;i++)
+        {
+            linkedList.AddAfter(linkedList.Last, tempList[i]);
+            linkedList.RemoveFirst();
+        }
+        return new List<int>(linkedList);
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int d = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        List<int> result = Result.rotLeft(a, d);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
